ddb4ece171175c53ed9a1ac06cd6bd77a2eac5c6
Stephan <cicchelli.stephan@gmail.com>
2023-08-10 16:53:21 -0300

    req4- notFound-refeito

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,17 +1,18 @@
 import { screen } from '@testing-library/react';
-import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-const renderAppWithTestRoute = () => {
-  renderWithRouter(<App />, { route: '/teste' });
-};
-test('Verifica se existe um h2 com o texto "Page requested not found"', () => {
-  renderAppWithTestRoute();
-  expect(screen.getByRole('heading', { name: 'Page requested not found' })).toBeInTheDocument();
+beforeEach(() => {
+  renderWithRouter(<App />, { route: '/errado' });
 });
 
-test('Verifica se a pagina possui uma imagem com o alt text "Clefairy pushing buttons randomly with text I have no idea what i`m doing"', () => {
-  renderAppWithTestRoute();
-  const notFound = screen.getByRole('img');
-  expect(notFound).toHaveAttribute('src', '/404.gif');
+test('Exibe um h2 com o texto "Page requested not found"', () => {
+  const headingNotFound = screen.getByRole('heading', { name: /Page requested not found/i });
+  expect(headingNotFound).toBeInTheDocument();
+});
+
+test("Existe uma imagem com o alt 'Clefairy pushing buttons randomly with text I have no idea what i'm doing'", () => {
+  const image = screen.getByRole('img');
+  const altText = "Clefairy pushing buttons randomly with text I have no idea what i'm doing";
+  expect(image).toHaveAttribute('alt', altText);
 });
