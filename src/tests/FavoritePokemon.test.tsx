6a88884df95add13fee3c4fc9ca6f7c8bfdc4018
Stephan <cicchelli.stephan@gmail.com>
2023-08-10 15:51:46 -0300

    req3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testes de Pokémons favoritos', () => {
+  test('Exibe mensagem de nenhum Pokémon favorito', () => {
+    renderWithRouter(<App />, { route: '/favorites' });
+    const noFavoriteMessage = screen.getByText(/no favorite pokémon found/i);
+    expect(noFavoriteMessage).toBeInTheDocument();
+  });
+
+  test('Exibe Pokémon favorito na lista de Favoritos', async () => {
+    const { user } = renderWithRouter(<App />);
+    const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
+
+    fireEvent.click(linkMoreDetails);
+    fireEvent.click(screen.getByRole('checkbox', { name: /pokémon favoritado\?/i }));
+    fireEvent.click(screen.getByRole('link', { name: /favorite pokémon/i }));
+
+    const pokemonImage = screen.getByRole('img', { name: /sprite/i });
+    expect(pokemonImage).toBeInTheDocument();
+  });
+});
